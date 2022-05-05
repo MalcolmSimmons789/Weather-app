e2bc2c4546b4a355e7bb19dd896d9666b584402c
MalcolmSimmons789 <Malcolm.Simmons789@yahoo.com>
2022-05-05 01:27:24 -0500

    added jquery to project

--- weather.js
+++ weather.js
@@ -15,8 +15,7 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, temp, humidity, speed);
-    document.querySelector('.city').innerText = 'weather in' + name;
+    document.querySelector('.city').innerText = 'Weather in ' + name;
     document.querySelector.src =
       'https://openweathermap.org/img/wn/' + icon + '.png';
     document.querySelector('.description').innerText = description;
@@ -24,5 +23,25 @@ let weather = {
     document.querySelector('.humidity').innerText =
       'Humidity: ' + humidity + '%';
     document.querySelector('.wind').innerText = 'Wind Speed: ' + speed + 'km/h';
+    document.querySelector('.weather').classList.remove('loading');
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector('.search-bar').value);
   },
 };
+
+document.querySelector('.search button').addEventListener('click', function () {
+  weather.search();
+});
+
+document
+  .querySelector('.search-bar')
+  .addEventListener('keyup', function (event) {
+    if (event.key == 'Enter') {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather('Denver');
